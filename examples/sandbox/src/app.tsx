de933edad0d6fb35eeab8ecbf8871a04465092d2
Damian Tarnawski <gthetarnav@gmail.com>
2023-07-08 12:08:47 +0200

    Drag drag drag

--- examples/sandbox/src/app.tsx
+++ examples/sandbox/src/app.tsx
@@ -1,5 +1,6 @@
-import type { Component } from 'solid-js'
+import { createSignal, type Component, For, createSelector, createEffect } from 'solid-js'
 import { graph, trig } from './lib'
+import clsx from 'clsx'
 
 export const App: Component = () => {
     const nodes: graph.GraphNode[] = [
@@ -8,17 +9,61 @@ export const App: Component = () => {
         new graph.GraphNode(new trig.Vector(35, -10)),
     ]
 
+    const [_track, trigger] = createSignal(undefined, { equals: false })
+    const track = <T,>(v: T) => (_track(), v)
+
+    const [dragging, setDragging] = createSignal<graph.GraphNode>()
+    let container!: HTMLDivElement
+
+    const isDragging = createSelector(dragging)
+
     return (
-        <div class="w-80vw h-80vw m-10vw bg-dark-9 relative">
-            {nodes.map((node, i) => (
-                <div
-                    class="absolute w-4 h-4 rounded-full bg-red -mt-2 -ml-2"
-                    style={{
-                        left: `${node.position.x + 50}%`,
-                        top: `${50 - node.position.y}%`,
-                    }}
-                ></div>
-            ))}
+        <div
+            ref={container}
+            class="w-80vw h-80vw m-10vw bg-dark-9 relative"
+            onMouseUp={e => {
+                e.preventDefault()
+                setDragging()
+            }}
+            onMouseLeave={e => {
+                e.preventDefault()
+                setDragging()
+            }}
+            onMouseMove={e => {
+                e.preventDefault()
+
+                const draggingNode = dragging()
+
+                if (draggingNode === undefined) return
+
+                const rect = container.getBoundingClientRect()
+                const x = (e.clientX - rect.left) / rect.width
+                const y = (e.clientY - rect.top) / rect.height
+                draggingNode.position.x = x * 100 - 50
+                draggingNode.position.y = 50 - y * 100
+
+                trigger()
+            }}
+        >
+            <For each={track(nodes)}>
+                {node => (
+                    <div
+                        class={clsx(
+                            'absolute w-5 h-5 rounded-full -mt-2.5 -ml-2.5',
+                            isDragging(node) ? 'bg-cyan' : 'bg-red',
+                        )}
+                        style={{
+                            left: `${track(node.position.x) + 50}%`,
+                            top: `${50 - track(node.position.y)}%`,
+                        }}
+                        onMouseDown={e => {
+                            if (dragging() !== undefined) return
+                            e.preventDefault()
+                            setDragging(node)
+                        }}
+                    ></div>
+                )}
+            </For>
         </div>
     )
 }
