fd962ceb376dfa3a96012ba721bfb314f87daba1
Damian Tarnawski <gthetarnav@gmail.com>
2023-07-21 18:57:38 +0200

    Add eslint config and update tsup config

--- packages/utils/tsup.config.ts
+++ packages/utils/tsup.config.ts
@@ -1,15 +1,35 @@
 import fs from 'fs'
 import path from 'path'
-import { defineConfig } from 'tsup-preset-solid'
+import { defineConfig } from 'tsup'
+import * as preset from 'tsup-preset-solid'
 
 const src = path.resolve(__dirname, 'src')
 const entries = fs.readdirSync(src)
 
-export default defineConfig(
-    entries.map(entry => ({ entry: path.join(src, entry) })),
-    {
-        // Enable this to write export conditions to package.json
-        writePackageJson: true,
-        dropConsole: true,
-    },
-)
+const preset_options: preset.PresetOptions = {
+    entries: entries.map(entry => ({ entry: path.join(src, entry) })),
+    drop_console: true,
+}
+
+const CI =
+    process.env['CI'] === 'true' ||
+    process.env['GITHUB_ACTIONS'] === 'true' ||
+    process.env['CI'] === '"1"' ||
+    process.env['GITHUB_ACTIONS'] === '"1"'
+
+export default defineConfig(config => {
+    const watching = !!config.watch
+
+    const parsed_options = preset.parsePresetOptions(preset_options, watching)
+
+    if (!watching && !CI) {
+        const package_fields = preset.generatePackageExports(parsed_options)
+
+        /*
+            will update ./package.json with the correct export fields
+        */
+        preset.writePackageJson(package_fields)
+    }
+
+    return preset.generateTsupOptions(parsed_options)
+})
