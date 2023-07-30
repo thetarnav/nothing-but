4f6f6efd316cb3ee7f40cc76ac31f66295e885c1
Damian Tarnawski <gthetarnav@gmail.com>
2023-07-30 19:39:30 +0200

    Switch to tsc for building (fix builds)

---
+++ .changeset/plenty-tools-tell.md
@@ -0,0 +1,6 @@
+---
+'@nothing-but/solid': patch
+'@nothing-but/utils': patch
+---
+
+Switch to tsc for building (fix builds)
--- packages/solid/tsup.config.ts
+++
@@ -1,3 +0,0 @@
-import * as configs from '../../configs/tsup.config'
-
-export default configs.generate_multi_entry_config(__dirname)
