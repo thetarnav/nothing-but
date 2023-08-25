b43498a8e9497aac0f63993dff2a5659dc53ac75
Damian Tarnawski <gthetarnav@gmail.com>
2023-08-25 18:01:06 +0200

    Fix force-graph test

--- packages/force-graph/test/index.test.tsx
+++ packages/force-graph/test/index.test.tsx
@@ -1,26 +1,16 @@
 import {Position} from '@nothing-but/utils/types'
 import {describe, expect, test} from 'vitest'
-import {
-    Graph,
-    Node,
-    Options,
-    default_options,
-    findClosestNode,
-    findClosestNodeLinear,
-    makeGraph,
-    makeGraphGrid,
-    makeNode,
-} from '../src/index.js'
+import {Graph} from '../src/index.js'
 
-const test_options: Options = {
-    ...default_options,
+const test_options: Graph.Options = {
+    ...Graph.default_options,
     grid_size: 40,
     repel_distance: 10,
 }
 
 describe('makeGraphGrid', () => {
     test('makeGraphGrid', () => {
-        const grid = makeGraphGrid(test_options)
+        const grid = Graph.graphGrid(test_options)
         expect(grid.axis_cells).toBe(4)
         expect(grid.cells.length).toBe(16)
         expect(grid.cell_size).toBe(10)
@@ -51,11 +41,11 @@ describe('find closest node', () => {
         0    10    20    30    40
     */
 
-    const node_a = makeNode('a')
-    const node_b = makeNode('b')
-    const node_c = makeNode('c')
-    const node_d = makeNode('d')
-    const node_e = makeNode('e')
+    const node_a = Graph.makeNode('a')
+    const node_b = Graph.makeNode('b')
+    const node_c = Graph.makeNode('c')
+    const node_d = Graph.makeNode('d')
+    const node_e = Graph.makeNode('e')
 
     node_a.position = {x: 11, y: 12}
     node_b.position = {x: 22, y: 13}
@@ -63,14 +53,18 @@ describe('find closest node', () => {
     node_d.position = {x: 26, y: 22}
     node_e.position = {x: 21, y: 29}
 
-    const graph = makeGraph(test_options, [node_a, node_b, node_c, node_d, node_e])
-
     const fns = {
-        findClosestNode,
-        findClosestNodeLinear(graph: Graph, pos: Position, max_dist?: number): Node | undefined {
-            return findClosestNodeLinear(graph.nodes, pos, max_dist)
+        findClosestNode: Graph.findClosestNode,
+        findClosestNodeLinear(
+            graph: Graph.Graph,
+            pos: Position,
+            max_dist?: number,
+        ): Graph.Node | undefined {
+            return Graph.findClosestNodeLinear(graph.nodes, pos, max_dist)
         },
     }
+
+    const graph = Graph.makeGraph(test_options, [node_a, node_b, node_c, node_d, node_e])
 
     Object.entries(fns).forEach(([name, fn]) => {
         describe(name, () => {
