ca389c5f419db7bdc638b7fdec2175220c2beb96
Damian Tarnawski <gthetarnav@gmail.com>
2023-11-19 17:39:48 +0100

    New package: matrix

---
+++ packages/matrix/src/array_type.ts
@@ -0,0 +1,13 @@
+export let array_constructor: typeof Float32Array | typeof Array =
+    typeof Float32Array !== 'undefined' ? Float32Array : Array
+
+/**
+ * Sets the global array constructor used when creating new vectors and matrices
+ *
+ * @param constructor Array type, such as Float32Array or Array
+ */
+export function set_matrix_array_constructor(
+    constructor: Float32ArrayConstructor | ArrayConstructor,
+): void {
+    array_constructor = constructor
+}
