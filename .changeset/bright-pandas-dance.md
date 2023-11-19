6a27bfa9ff1fd6f7a955aa33dfd9635d3a394996
Damian Tarnawski <gthetarnav@gmail.com>
2023-11-19 17:39:32 +0100

    utils: Add to_radian to num

---
+++ .changeset/bright-pandas-dance.md
@@ -0,0 +1,5 @@
+---
+'@nothing-but/utils': minor
+---
+
+Add to_radian to num
--- packages/utils/src/num.ts
+++ packages/utils/src/num.ts
@@ -54,9 +54,40 @@ export function to_percent(value: number, min: number, max: number): number {
     return (value - min) / (max - min)
 }
 
-export function equals(a: number, b: number): boolean {
-    return Math.abs(a - b) < Number.EPSILON
+/**
+ * Symmetric round
+ * see https://www.npmjs.com/package/round-half-up-symmetric#user-content-detailed-background
+ *
+ * @param a value to round
+ */
+export function round(a: number): number {
+    return a >= 0 || a % 0.5 !== 0 ? Math.round(a) : Math.floor(a)
 }
+
+const DEGREE: number = Math.PI / 180
+
+/**
+ * Convert Degree To Radian
+ *
+ * @param a Angle in Degrees
+ */
+export function to_radian(a: number): number {
+    return a * DEGREE
+}
+
+/**
+ * Tests whether or not the arguments have approximately the same value, within an absolute
+ * or relative tolerance of Number.EPSILON (an absolute tolerance is used for values less
+ * than or equal to 1.0, and a relative tolerance is used for larger values)
+ *
+ * @param a The first number to test.
+ * @param b The second number to test.
+ * @returns True if the numbers are approximately equal, false otherwise.
+ */
+export function equals(a: number, b: number): boolean {
+    return Math.abs(a - b) <= Number.EPSILON * Math.max(1.0, Math.abs(a), Math.abs(b))
+}
+
 export function between(a: number, b: number, c: number): boolean {
     if (a > c) [a, c] = [c, a]
     return a - Number.EPSILON <= b && b <= c + Number.EPSILON
