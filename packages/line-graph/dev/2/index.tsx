df673dab164b2dc375741e4f33863a6ae6c7e279
Damian Tarnawski <gthetarnav@gmail.com>
2023-11-27 17:42:42 +0100

    Line with thickness

--- packages/line-graph/dev/2/index.tsx
+++ packages/line-graph/dev/2/index.tsx
@@ -56,18 +56,24 @@ export const App: solid.Component = () => {
 
     gl.useProgram(program)
 
-    const a_position = gl.getAttribLocation(program, 'a_position')
-    const a_color = gl.getAttribLocation(program, 'a_color')
     const u_resolution = gl.getUniformLocation(program, 'u_resolution')
+    const u_thickness = gl.getUniformLocation(program, 'u_thickness')
+
+    const a_position = gl.getAttribLocation(program, 'a_position')
+    const a_angle = gl.getAttribLocation(program, 'a_angle')
+    const a_color = gl.getAttribLocation(program, 'a_color')
 
     // Turn on the attribute
     gl.enableVertexAttribArray(a_position)
+    gl.enableVertexAttribArray(a_angle)
     gl.enableVertexAttribArray(a_color)
 
     const positions_buffer = gl.createBuffer()
+    const angles_buffer = gl.createBuffer()
     const colors_buffer = gl.createBuffer()
 
-    if (!positions_buffer || !colors_buffer) throw new Error('failed to create gl buffers')
+    if (!positions_buffer || !colors_buffer || !angles_buffer)
+        throw new Error('failed to create gl buffers')
 
     // prettier-ignore
     const data_points = new Float32Array([
@@ -77,15 +83,28 @@ export const App: solid.Component = () => {
         600, 150,
         800, 400,
     ])
+
+    const EASING_FREQUENCY = 32
+
     const data_points_count = data_points.length / 2
-
-    const EASING_FREQUENCY = 12
-
     // fill in the gaps between data points
     const easing_points_count = (data_points_count - 1) * EASING_FREQUENCY
-    const easing_points_length = easing_points_count * 2
     const positions_count = easing_points_count * 2 // add the inset points
+
+    /*
+    BUFFERS
+
+    positions: (2 points per easing point for adding thickness)
+    [x1, y1]    [x1, y1]...
+    angles: (normals)
+    [rad]       [rad]...
+    colors:
+    [r, g, b, a][r, g, b, a]...
+
+    */
     const positions = new Float32Array(positions_count * 2)
+    const angles = new Float32Array(positions_count)
+    const colors = new Uint8Array(positions_count * 4)
 
     /*
     EASING
@@ -103,71 +122,59 @@ export const App: solid.Component = () => {
             const x = utils.num.lerp(x1, x2, p)
             const y = utils.num.lerp(y1, y2, ease)
 
-            const easing_idx = data_idx * EASING_FREQUENCY + (j << 1)
-            positions[easing_idx] = x
+            const easing_idx = data_idx * (EASING_FREQUENCY << 1) + (j << 2)
+            /*
+            2 points per easing point (for adding thickness)
+            */
+            positions[easing_idx + 0] = x
             positions[easing_idx + 1] = y
+            positions[easing_idx + 2] = x
+            positions[easing_idx + 3] = y
         }
     }
 
     /*
-    INSET POINTS
+    ANGLES (for normals)
     */
-    const INSET_WIDTH = 12
     for (let i = 1; i < easing_points_count - 1; i += 1) {
-        const idx = i << 1
-        const x = positions[idx]!
-        const y = positions[idx + 1]!
+        const pos_idx = i << 2 // 2 components, 2 points per easing point
 
-        const x1 = positions[idx - 2]!
-        const y1 = positions[idx - 1]!
-        const x2 = positions[idx + 2]!
-        const y2 = positions[idx + 3]!
+        const x1 = positions[pos_idx - 2]!
+        const y1 = positions[pos_idx - 1]!
+        const x2 = positions[pos_idx + 2]!
+        const y2 = positions[pos_idx + 3]!
 
-        const angle = Math.atan2(y2 - y1, x2 - x1) - Math.PI / 2
-
-        const inset_x = x + Math.cos(angle) * INSET_WIDTH
-        const inset_y = y + Math.sin(angle) * INSET_WIDTH
-
-        const inset_idx = easing_points_length + idx
-        positions[inset_idx] = inset_x
-        positions[inset_idx + 1] = inset_y
+        const angle = Math.atan2(y2 - y1, x2 - x1)
+        const angle_idx = i << 1 // 1 angle per point, 2 points per easing point
+        angles[angle_idx + 0] = angle - Math.PI / 2
+        angles[angle_idx + 1] = angle + Math.PI / 2
     }
-
-    /* first inset point */
-    positions[easing_points_length] = positions[0]!
-    positions[easing_points_length + 1] = positions[1]! - INSET_WIDTH
-
-    /* last inset point */
-    positions[easing_points_length + easing_points_length - 2] =
-        positions[easing_points_length - 2]!
-    positions[easing_points_length + easing_points_length - 1] =
-        positions[easing_points_length - 1]! - INSET_WIDTH
+    /* end points */
+    angles[0] = angles[positions_count - 2] = -Math.PI / 2
+    angles[1] = angles[positions_count - 1] = Math.PI / 2
 
     /*
     COLORS
     */
-    const colors = new Uint8Array(positions_count * 4)
-    {
-        let i = 0
-        let stop = easing_points_count * 4
-        for (; i < stop; i += 4) {
-            colors[i] = GREEN[0]
-            colors[i + 1] = GREEN[1]
-            colors[i + 2] = GREEN[2]
-            colors[i + 3] = GREEN[3]
-        }
-        stop += easing_points_count * 4
-        for (; i < stop; i += 4) {
-            colors[i] = RED[0]
-            colors[i + 1] = RED[1]
-            colors[i + 2] = RED[2]
-            colors[i + 3] = RED[3]
-        }
+    for (let i = 0; i < easing_points_count; i += 1) {
+        const color = randomColor()
+        const idx = i << 3
+        colors[idx + 0] = color[0]
+        colors[idx + 1] = color[1]
+        colors[idx + 2] = color[2]
+        colors[idx + 3] = color[3]
+        colors[idx + 4] = color[0]
+        colors[idx + 5] = color[1]
+        colors[idx + 6] = color[2]
+        colors[idx + 7] = color[3]
     }
 
     const loop = utils.raf.makeAnimationLoop(() => {
         // set the resolution
         gl.uniform2f(u_resolution, gl.canvas.width, gl.canvas.height)
+
+        // set the thickness
+        gl.uniform1f(u_thickness, 16)
 
         // Tell WebGL how to convert from clip space to pixels
         gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
@@ -180,11 +187,15 @@ export const App: solid.Component = () => {
         gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW)
         gl.vertexAttribPointer(a_position, 2, gl.FLOAT, false, 0, 0)
 
+        gl.bindBuffer(gl.ARRAY_BUFFER, angles_buffer)
+        gl.bufferData(gl.ARRAY_BUFFER, angles, gl.STATIC_DRAW)
+        gl.vertexAttribPointer(a_angle, 1, gl.FLOAT, false, 0, 0)
+
         gl.bindBuffer(gl.ARRAY_BUFFER, colors_buffer)
         gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW)
         gl.vertexAttribPointer(a_color, 4, gl.UNSIGNED_BYTE, true, 0, 0)
 
-        gl.drawArrays(gl.POINTS, 0, positions_count)
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, positions_count)
     })
     utils.raf.loopStart(loop)
     s.addCleanup(loop, utils.raf.loopClear)
