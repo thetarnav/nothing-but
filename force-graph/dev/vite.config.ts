f803581282d74a0db1b1fad788724e20da7dbd50
Damian Tarnawski <gthetarnav@gmail.com>
2024-09-11 14:54:42 +0200

    Correct vite for gh pages

--- force-graph/dev/vite.config.ts
+++ force-graph/dev/vite.config.ts
@@ -4,6 +4,7 @@ import {defineConfig} from "vite"
 import uno_config from "../../uno.config.js"
 
 export default defineConfig({
+	base: "",
 	server: {port: 3000},
 	plugins: [solid(), unocss(uno_config)],
 	build: {
